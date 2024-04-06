06061663a1b90543e824807fa1d7c36617533810
mariansimecek <mariansimecek@gmail.com>
2024-04-06 23:13:44 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script >
+  import TextInput from './TextInput.svelte';
+
+  let value = $state('');
+  const _snippet = snippet
+</script>
+
+{#snippet snippet()}
+  Something
+{/snippet}
+
+<TextInput bind:value />
+value:
+{value}
